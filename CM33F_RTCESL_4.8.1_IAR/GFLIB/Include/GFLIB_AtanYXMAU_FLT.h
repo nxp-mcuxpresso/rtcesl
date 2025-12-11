4505465793b9d6c1b1b3b5a6f7f481811ec8ffa3
nxa18911 <peter.butko@nxp.com>
2025-12-11 15:53:03 +0100

    [FSLESL-612] Added result register address structure

--- CM33F_RTCESL_4.8.1_IAR/GFLIB/Include/GFLIB_AtanYXMAU_FLT.h
+++ CM33F_RTCESL_4.8.1_IAR/GFLIB/Include/GFLIB_AtanYXMAU_FLT.h
@@ -54,7 +54,7 @@ extern "C" {
 *
 * @param     in  r0 - fltY in the range of [-1,1).
 *                r1 - fltX in the range of [-1,1).
-*                r3 - u8ResReg - MAU result register.
+*                r2 - u8ResReg - MAU result register.
 *
 * @return    The function returns
 *                r0 - AtanYX(fltY,fltX).The output data value
